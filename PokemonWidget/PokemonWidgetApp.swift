93aa8def8d09334fcb7c9fac74abe37582620464
muhammed gül <muhammedgul.mail@gmail.com>
2022-10-31 14:10:58 +0300

    Initial Commit

---
+++ PokemonWidget/PokemonWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWidgetApp.swift
+//  PokemonWidget
+//
+//  Created by Muhammed Gül on 31.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
